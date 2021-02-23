3481c7db5388b675f0da5b0a42e4a258666655d0
Michael Hanzel <41654851+Mikahanz@users.noreply.github.com>
2021-02-23 14:24:24 -0500

    Custom page titles & meta

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -26,12 +26,12 @@ const ProductCarousel = () => {
     <Carousel pause='hover' className='bg-dark'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product.id}`}>
+          <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
-              <h2>
+              <h3>
                 {product.name} ${product.price}
-              </h2>
+              </h3>
             </Carousel.Caption>
           </Link>
         </Carousel.Item>
